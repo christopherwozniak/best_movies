164693f9131e738912752e0246523558f4ecc6df
Krzysztof Woźniak <wozniak.krzysztof124@gmail.com>
2023-01-02 12:17:50 +0100

    Add login button on login_page

--- lib/app/home/login_page.dart
+++ lib/app/home/login_page.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({
     Key? key,
   }) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final emailController = TextEditingController();
+
+  final passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -13,9 +22,25 @@ class LoginPage extends StatelessWidget {
           padding: const EdgeInsets.all(20.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              TextField(),
-              TextField(obscureText: true),
+            children: [
+              const Text('Zaloguj się'),
+              const SizedBox(
+                height: 20,
+              ),
+              TextField(
+                controller: emailController,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextField(controller: passwordController, obscureText: true),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Zaloguj się'),
+              ),
             ],
           ),
         ),
