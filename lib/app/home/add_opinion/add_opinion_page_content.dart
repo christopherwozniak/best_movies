79b3460d5c5da1463b7baf4d659e2a681a0cbc4f
Krzysztof Woźniak <wozniak.krzysztof124@gmail.com>
2023-01-09 12:39:22 +0100

    Implement adding movies and actors

--- lib/app/home/add_opinion/add_opinion_page_content.dart
+++ lib/app/home/add_opinion/add_opinion_page_content.dart
@@ -1,22 +1,56 @@
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 
 
 
-class AddOpinionPageContent extends StatelessWidget {
+class AddOpinionPageContent extends StatefulWidget {
   const AddOpinionPageContent({
     Key? key,
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<Object>(
-        stream: FirebaseFirestore.instance.collection('movies').snapshots(),
-        builder: (context, snapshot) {
-          return const Center(
-            child: Text('Dwa'),
-          );
-        });
-  }
+  State<AddOpinionPageContent> createState() => _AddOpinionPageContentState();
 }
+
+class _AddOpinionPageContentState extends State<AddOpinionPageContent> {
+
+  var movieName= '';
+  var actorName= '';
+  @override
+  Widget build(BuildContext context) {
+    {
+          return  Center(
+            child: Column(mainAxisAlignment: MainAxisAlignment.center,
+              children:  [
+                 TextField(
+                  decoration: const InputDecoration(hintText: 'Give the movie name',), onChanged: (newValue) {
+                  setState(() {
+                    movieName = newValue;
+                    
+                  });
+
+                 }),const SizedBox(height: 20,),
+                 TextField(decoration: const InputDecoration(hintText: 'Give the actor name',), onChanged: (newValue) {
+                  setState(() {
+                    actorName = newValue;
+                    
+                  });
+
+                 }),const SizedBox(height: 20,),
+                 ElevatedButton(onPressed: () {
+                  FirebaseFirestore.instance.collection('movies',).add({
+                    'name':movieName,
+                    'actor':actorName,
+                    'rating': 3.0,
+
+                  });
+                 }, child: const Text('Add opinion'),),
+                
+  ],
+            ),
+          );
+        
+  }
+}}
