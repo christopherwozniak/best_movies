4460f771fe42924119aff59696a9432cc9c33b94
Krzysztof Woźniak <wozniak.krzysztof124@gmail.com>
2023-01-09 13:36:07 +0100

    Change BottomNavigationBar to 0 on save

--- lib/app/home/add_opinion/add_opinion_page_content.dart
+++ lib/app/home/add_opinion/add_opinion_page_content.dart
@@ -8,7 +8,10 @@ import 'package:flutter/material.dart';
 class AddOpinionPageContent extends StatefulWidget {
   const AddOpinionPageContent({
     Key? key,
+    required this.onSave,
   }) : super(key: key);
+
+  final Function onSave;
 
   @override
   State<AddOpinionPageContent> createState() => _AddOpinionPageContentState();
@@ -23,42 +26,47 @@ class _AddOpinionPageContentState extends State<AddOpinionPageContent> {
   Widget build(BuildContext context) {
     {
           return  Center(
-            child: Column(mainAxisAlignment: MainAxisAlignment.center,
-              children:  [
-                 TextField(
-                  decoration: const InputDecoration(hintText: 'Give the movie name',), onChanged: (newValue) {
-                  setState(() {
-                    movieName = newValue;
-                    
-                  });
+            child: Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Column(mainAxisAlignment: MainAxisAlignment.center,
+                children:  [
+                   TextField(
+                    decoration: const InputDecoration(hintText: 'Give the movie name',), onChanged: (newValue) {
+                    setState(() {
+                      movieName = newValue;
+                      
+                    });
 
-                 }),const SizedBox(height: 20,),
-                 TextField(decoration: const InputDecoration(hintText: 'Give the actor name',), onChanged: (newValue) {
-                  setState(() {
-                    actorName = newValue;
-                    
-                  });
+                   }),const SizedBox(height: 20,),
+                   TextField(decoration: const InputDecoration(hintText: 'Give the actor name',), onChanged: (newValue) {
+                    setState(() {
+                      actorName = newValue;
+                      
+                    });
 
-                 }),const SizedBox(height: 20,),
-                 Slider(onChanged: (newValue) {
-                  setState(() {
-                    rating= newValue;
-                  });
-                 }, value: rating,
-                 min: 0.0,
-                 max: 10.0,
-                 divisions: 10,
-                 label: rating.toString(),),
-                 ElevatedButton(onPressed: () {
-                  FirebaseFirestore.instance.collection('movies',).add({
-                    'name':movieName,
-                    'actor':actorName,
-                    'rating': rating,
+                   }),const SizedBox(height: 20,),
+                   Slider(onChanged: (newValue) {
+                    setState(() {
+                      rating= newValue;
+                    });
+                   }, value: rating,
+                   min: 0.0,
+                   max: 10.0,
+                   divisions: 10,
+                   label: rating.toString(),),
+                   ElevatedButton(onPressed: movieName.isEmpty || actorName.isEmpty ? null :  () 
+                   { 
+                    FirebaseFirestore.instance.collection('movies',).add({
+                      'name':movieName,
+                      'actor':actorName,
+                      'rating': rating,
 
-                  });
-                 }, child: const Text('Add opinion'),),
-                
+                    });
+                    widget.onSave();
+                   }, child: const Text('Add opinion'),),
+                  
   ],
+              ),
             ),
           );
         
