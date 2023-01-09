44997dae30d542b34ff33924c81dbaac01a0aa13
Krzysztof Woźniak <wozniak.krzysztof124@gmail.com>
2023-01-09 15:45:07 +0100

    Create required documents in MoviesState

--- lib/app/home/movies/cubit/movies_cubit.dart
+++ lib/app/home/movies/cubit/movies_cubit.dart
@@ -1,8 +1,9 @@
 import 'package:bloc/bloc.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:meta/meta.dart';
 
 part 'movies_state.dart';
 
 class MoviesCubit extends Cubit<MoviesState> {
-  MoviesCubit() : super(MoviesState());
+  MoviesCubit() : super( MoviesState (documents: const [],),);
 }
--- lib/app/home/movies/cubit/movies_state.dart
+++ lib/app/home/movies/cubit/movies_state.dart
@@ -1,6 +1,10 @@
 part of 'movies_cubit.dart';
 
 @immutable
- class MoviesState {}
+ class MoviesState {
+  final List<QueryDocumentSnapshot<Map<String, dynamic>>> documents;
+
+   MoviesState ({required this.documents});
+ }
 
 
