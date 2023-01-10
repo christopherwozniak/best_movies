1abace9294448ec3ed2a608734a54a107bc54ca4
Krzysztof Woźniak <wozniak.krzysztof124@gmail.com>
2023-01-10 11:27:00 +0100

    Create app.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:best_movies/app/app.dart';
 import 'package:best_movies/app/root/root_page.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -11,17 +12,3 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.brown,
-      ),
-      home: const RootPage(),
-    );
-  }
-}
