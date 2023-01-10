961691635c2d02c905e20521fe56b93c5d694dfb
Krzysztof Woźniak <wozniak.krzysztof124@gmail.com>
2023-01-10 11:28:02 +0100

    Delete unusual imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:best_movies/app/app.dart';
-import 'package:best_movies/app/root/root_page.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
