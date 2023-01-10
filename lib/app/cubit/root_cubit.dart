bb08b8bbd254418e40b2a84085599195037ac136
Krzysztof Woźniak <wozniak.krzysztof124@gmail.com>
2023-01-10 11:33:15 +0100

    Add RootState to root_cubit

---
+++ lib/app/cubit/root_cubit.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: depend_on_referenced_packages
+
+import 'package:bloc/bloc.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:meta/meta.dart';
+
+part 'root_state.dart';
+
+class RootCubit extends Cubit<RootState> {
+  RootCubit() : super(const RootState(user: null));
+}
